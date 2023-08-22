a7c74ea331bbc2f327469fc300eb5e16a37625dc
EphriamHab <ephremhabtamu2015@gmail.com>
2023-08-22 22:05:05 +0300

    backend modified

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./router/.env" })
+mongoose.connect(process.env.CONNECTION_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen("5000",() => {
     console.log("backend running");
 })
