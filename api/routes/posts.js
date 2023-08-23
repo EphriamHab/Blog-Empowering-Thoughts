4ae88b4db76b60f075f1a909d6c42b289aeb8d0a
EphriamHab <ephremhabtamu2015@gmail.com>
2023-08-23 20:15:07 +0300

    backend  post.js

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -15,8 +15,67 @@ router.post("/", async(req,res)=>{
 router.put("/:id", async(req,res)=>{
     try{
      const post = await Post.findById(req.params.id)
+     if(post.username === req.body.username){
+        try{
+            const upadtePost = await Post.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set :req.body
+                },
+                {
+                    new:true,
+                }
+            )
+            res.status(200).json(upadtePost)
+        }catch(error){
+            res.status(200).json(error)
+        }
+     }else{
+        res.status(401).json("You can update only your post!")
+     }
     }catch(error){
-
+        res.status(500).json(error)
     }
+})
+// delete
+router.delete("/:id", async(req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id)
+        if(post.username === req.body.username){
+          try {
+            await post.delete()
+            res.status(200).json("post has been deleted")
+
+          } catch (error) {
+            res.status(500).json(error)
+          }
+        }else{
+            res.status(401).json("You can delete only your post!")  
+        }
+    }catch(error){
+        res.status(500).json(error)
+    }
+})
+// get post
+router.get("/", async(req,res)=>{
+const username = req.query.user
+const catName = req.query.cat
+try {
+    let posts
+    if(username){
+        posts = await Post.find({username:username})
+    }else if(catName){
+        posts = await Post.find({
+        categories:{
+            $in: [catName],
+        },
+            })
+    }else{
+        posts = await Post.find()
+    }
+    res.status(404).json(posts)
+} catch (error) {
+    res.status(404).json(error) 
+}
 })
 module.exports = router
